eda45e95ab60a8ff902eeddd59fec540324c374f
cz-czan <marcel.kantor@protonmail.com>
2021-09-19 18:18:03 +0200

    Initial commit.

---
+++ Scripts/NameGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class NameGenerator
+{
+    public string generateName()
+    {
+        string praenomen;
+        string nomen;
+        
+    }
+}
+ 
---
+++ Scripts/Senator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Senator : Citizen
+{
+    public int rank;
+    
+
+    public Senator()
+    {
+        
+    }
+}
